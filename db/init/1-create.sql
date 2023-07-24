99711f57d982865bdb165c97eb44cac26d9fda79
“dev-sunnn” <dev@sunnn.jp>
2023-07-24 23:30:25 +0900

    sql update

--- db/init/1-create.sql
+++ db/init/1-create.sql
@@ -1,15 +1,150 @@
 -- DB切り替え
 \c postgres_db_sample;
 
--- テーブルを削除
-DROP TABLE IF EXISTS "sample";
+-- sample_table テーブルを削除
+DROP TABLE IF EXISTS "sample_table";
 
--- sample テーブル作成
+-- sample_table テーブル作成
 CREATE TABLE "sample_table" (
-    "id" serial PRIMARY KEY,
-    "name" VARCHAR( 100 ),
-    "update_date" TIMESTAMP,
-    "create_date" TIMESTAMP,
-    "delete_date" TIMESTAMP
+  "id" serial PRIMARY KEY,
+  "name" VARCHAR( 100 ),
+  "update_date" TIMESTAMP,
+  "create_date" TIMESTAMP,
+  "delete_date" TIMESTAMP
+);
+
+-- m_commodity テーブルを削除
+DROP TABLE IF EXISTS m_commodity;
+
+-- m_commodity テーブル作成
+CREATE TABLE m_commodity
+(
+  "commodity_cd" CHAR(2) PRIMARY KEY,
+  "commodity_name" VARCHAR(50) NOT NULL,
+  "price" INTEGER,
+  "delete_flg" CHAR(1),
+  "update_user" VARCHAR(20),
+  "update_date" TIMESTAMP,
+  "create_user" VARCHAR(20),
+  "create_date" TIMESTAMP
+);
+
+-- m_customer テーブルを削除
+DROP TABLE IF EXISTS m_customer;
+
+-- m_customer テーブル作成
+CREATE TABLE m_customer
+(
+  "customer_id" CHAR(2) PRIMARY KEY,
+  "customer_name" VARCHAR(30) NOT NULL,
+  "gender" CHAR(3),
+  "age_a" CHAR(2),
+  "delete_flg" CHAR(1),
+  "update_user" VARCHAR(20),
+  "update_date" TIMESTAMP,
+  "create_user" VARCHAR(20),
+  "create_date" TIMESTAMP
+);
+
+-- t_order_tbl テーブルを削除
+DROP TABLE IF EXISTS t_order_tbl;
+
+-- t_order_tbl テーブル作成
+CREATE TABLE t_order_tbl
+(
+  "order_no" CHAR(8) PRIMARY KEY,
+  "commodity_cd" CHAR(2),
+  "quantity" INTEGER,
+  "customer_id" CHAR(2),
+  "delete_flg" CHAR(1),
+  "update_user" VARCHAR(20),
+  "update_date" TIMESTAMP,
+  "create_user" VARCHAR(20),
+  "create_date" TIMESTAMP
+);
+
+-- sample_product テーブルを削除
+DROP TABLE IF EXISTS sample_product;
+
+-- sample_product テーブル作成
+CREATE TABLE sample_product
+(
+  "sample_product_cd" CHAR(2) PRIMARY KEY,
+  "sample_name" VARCHAR(50) NOT NULL,
+  "price" INTEGER,
+  "delete_flg" CHAR(1),
+  "update_user" VARCHAR(20),
+  "update_date" TIMESTAMP,
+  "create_user" VARCHAR(20),
+  "create_date" TIMESTAMP
+);
+
+-- sample_product_2 テーブルを削除
+DROP TABLE IF EXISTS sample_product_2;
+
+-- sample_product_2 テーブル作成
+CREATE TABLE sample_product_2
+(
+  "sample_product_2_cd" CHAR(2) PRIMARY KEY,
+  "sample_name" VARCHAR(50) NOT NULL,
+  "size" INTEGER,
+  "price" INTEGER,
+  "delete_flg" CHAR(1),
+  "update_user" VARCHAR(20),
+  "update_date" TIMESTAMP,
+  "create_user" VARCHAR(20),
+  "create_date" TIMESTAMP
+);
+
+-- sample_product_3 テーブルを削除
+DROP TABLE IF EXISTS sample_product_3;
+
+-- sample_product_3 テーブル作成
+CREATE TABLE sample_product_3
+(
+  "sample_product_3_cd" CHAR(2) PRIMARY KEY,
+  "sample_name" VARCHAR(50) NOT NULL,
+  "category" VARCHAR(50),
+  "price" INTEGER,
+  "qty" INTEGER,
+  "delete_flg" CHAR(1),
+  "update_user" VARCHAR(20),
+  "update_date" TIMESTAMP,
+  "create_user" VARCHAR(20),
+  "create_date" TIMESTAMP
+);
+
+-- sample_product_4 テーブルを削除
+DROP TABLE IF EXISTS sample_product_4;
+
+-- sample_product_4 テーブル作成
+CREATE TABLE sample_product_4
+(
+  "sample_product_4_cd" CHAR(2) PRIMARY KEY,
+  "sample_name" VARCHAR(50) NOT NULL,
+  "price" INTEGER,
+  "qty" INTEGER,
+  "delete_flg" CHAR(1),
+  "update_user" VARCHAR(20),
+  "update_date" TIMESTAMP,
+  "create_user" VARCHAR(20),
+  "create_date" TIMESTAMP
+);
+
+-- sample_product_5 テーブルを削除
+DROP TABLE IF EXISTS sample_product_5;
+
+-- sample_product_5 テーブル作成
+CREATE TABLE sample_product_5
+(
+  "sample_product_5_cd" CHAR(2) PRIMARY KEY,
+  "sample_name" VARCHAR(50) NOT NULL,
+  "price" INTEGER,
+  "qty" INTEGER,
+  "delete_flg" CHAR(1),
+  "update_user" VARCHAR(20),
+  "update_date" TIMESTAMP,
+  "create_user" VARCHAR(20),
+  "create_date" TIMESTAMP
 );
 
--- db/init/2-insert.sql
+++ db/init/2-insert.sql
@@ -1,7 +1,80 @@
--- サンプル情報登録
+-- sample_table 登録
 INSERT INTO "sample_table"
 VALUES 
 ('1', 'サンプル情報 1', '2023/07/26', '2023/07/26', NULL),
 ('2', 'サンプル情報 2', '2023/07/26', '2023/07/26', NULL),
 ('3', 'サンプル情報 3', '2023/07/26', '2023/07/26', NULL),
 ('4', 'サンプル情報 4', '2023/07/26', '2023/07/26', NULL);
+
+-- m_commodity 登録
+INSERT INTO "m_commodity"
+VALUES 
+('01', '牛カルビ', 800, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('02', '牛タン塩', 700, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('03', '豚トロ', 500, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('04', '豚タン塩', 600, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('05', '上牛カルビ', 1200, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('06', 'ウィンナー', null, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26');
+
+-- m_customer 登録
+INSERT INTO "m_customer"
+VALUES 
+('A1', '竹本　ゆき', '女', 3, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('A2', '清水　聖人', '男', 25, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('A3', 'Taylor Alison Swift', '女', 22, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('A4', '石郷岡　英明', '男', 48, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26');
+
+-- t_order_tbl 登録
+INSERT INTO "t_order_tbl"
+VALUES 
+('ODR00001', '05', 3, 'A1', '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('ODR00002', '01', 2, 'A1', '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('ODR00003', '03', 2, 'A3', '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('ODR00004', '02', 2, 'A4', '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('ODR00005', '05', 1, 'A3', '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('ODR00006', '03', 5, 'A2', '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26');
+
+-- sample_product 登録
+INSERT INTO "sample_product"
+VALUES 
+('01', 'AAA', 10000, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('02', 'BBB', 8500, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('03', 'CCC', 900, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('04', 'DDD', null, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('05', 'EEE', 5000,'0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('06', 'FFF', 4900,'0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('07', 'GGG', 100,'0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26');
+
+-- sample_product_2 登録
+INSERT INTO "sample_product_2"
+VALUES 
+('01', 'HHH', 100, 10000, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('02', 'III', 200, 18000, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('03', 'JJJ', 300, 27000, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('04', 'KKK', 400, 40000, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('05', 'LLL', 500, 52000,'0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26');
+
+-- sample_product_3 登録
+INSERT INTO "sample_product_3"
+VALUES 
+('01', 'AAA', null, 2000, 300, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('02', 'BBB', null, 1200, 200, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('03', 'CCC', null, 900, 100, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('04', 'DDD', 'サンプル', 120, 47, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('05', 'EEE', 'サンプル', 200, 51, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('06', 'FFF', 'サンプル', 150, 60, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26');
+
+-- sample_product_4 登録
+INSERT INTO "sample_product_4"
+VALUES 
+('01', 'AAA', 120, 47, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('02', 'BBB', 200, 51, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('03', 'CCC', 150, 60, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26');
+
+-- sample_product_5 登録
+INSERT INTO "sample_product_5"
+VALUES 
+('01', 'AAA', 2000, 20, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('02', 'BBB', 4870, 0, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('03', 'CCC', 9800, 0, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26'),
+('04', 'DDD', 6410, 6, '0', 'UPDATE_USER', '2023/07/26', 'CREATE_USER', '2023/07/26');
